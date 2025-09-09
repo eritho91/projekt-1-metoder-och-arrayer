3f45aeefedf22ffa7d5dd13c2dc1a84b892e1346
eritho91 <erik.thorell@iths.se>
2025-09-09 12:06:51 +0200

    Test

--- src/main/java/se/iths/erikthorell/projekt1/Launch.java
+++ src/main/java/se/iths/erikthorell/projekt1/Launch.java
@@ -1,4 +1,8 @@
 package se.iths.erikthorell.projekt1;
 
 public class Launch {
+    public static void main(String[] args) {
+        Logic logic = new Logic();
+        logic.findLargestNumber();
+    }
 }
--- src/main/java/se/iths/erikthorell/projekt1/Logic.java
+++ src/main/java/se/iths/erikthorell/projekt1/Logic.java
@@ -1,4 +1,33 @@
 package se.iths.erikthorell.projekt1;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Logic {
+
+    public void findLargestNumber() {
+        Random random = new Random();
+        ArrayList<Integer> randomNumberList = new ArrayList<>();
+        int largestNumber = 0;
+        for(int i = 1; i <= 10; i++) {
+            randomNumberList.add(random.nextInt(100));
+        }
+        System.out.print("Genererade tal: ");
+        for(int num : randomNumberList) {
+            System.out.print(num + " ");
+        }
+        for(int num : randomNumberList) {
+            if(num > largestNumber) {
+                largestNumber = num;
+            }
+        }
+        System.out.println("\nStörsta talet är: " + largestNumber);
+    }
+
+    public void printSyntaxLoop(){
+        String[] syntax = {"Hello", " ", "World", "!"};
+        for(String s : syntax){
+            System.out.print(s);
+        }
+    }
 }
